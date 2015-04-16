09c14add32f18515fb27fba4272e50ce69e5f881
Blankpanda <Blankpanda8@gmail.com>
2015-04-16 07:55:47 -0400

    initial configuration
    added project

---
+++ dbtest/IntializeDatabase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace dbtest
+{
+
+    class IntializeDatabase
+    {
+        private static SQLiteConnection M_CONNECTION;
+
+
+        /*
+         * This method opens the connection to the data base 
+         * this database is currently found in the working directory
+         */
+        public void openDataBase()
+        {
+            var openString = "data source=elements.db";
+            M_CONNECTION = new SQLiteConnection(openString);
+            M_CONNECTION.Open();
+        }
+
+
+        /*
+         *This method will run a simple test query to the database called elements 
+         */
+        public dynamic queryDatabase(string inp)
+        {
+         
+            string sqlQuery = "SELECT * FROM elements WHERE Symbol LIKE " + inp;
+            SQLiteCommand command = new SQLiteCommand(sqlQuery,M_CONNECTION);
+            return command;
+        }
+
+        /*
+         * this method reads the database and stores the contents of the query
+         * in a string, which is passed back to main 
+         */
+        public dynamic readDatabase(SQLiteCommand c)
+        {
+            SQLiteDataReader reader = c.ExecuteReader();
+    
+            List<string> elements = new List<string>(); // used to store the elements being passed from the database
+           
+
+            while (reader.Read()) //reads the database based on the query
+            {
+
+                elements.Add("Element: " + reader["Name"]);
+               
+            }
+
+            return elements;
+
+
+        }
+    }
+}
---
+++ dbtest/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// /a/ > /g/
+
+namespace dbtest
+{
+    class Program 
+    {
+       private static IntializeDatabase DB;
+       private static SQLiteCommand QueryCommand;
+       private static List<string> elements;
+
+       
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type in a elements symbol: ");
+            string inp = Console.ReadLine();
+            inp = "'" + inp + "'";
+
+
+            DB = new IntializeDatabase(); //initalize the data base object
+            DB.openDataBase(); // open the database in working directory
+            QueryCommand = new SQLiteCommand(); // initalize the command to pass into the reader
+            QueryCommand = DB.queryDatabase(inp); // query the database and pass the query to the reader
+            elements = new List<string>(); // initalize list for database elements
+            elements = DB.readDatabase(QueryCommand); // readDatabase returns read data as a list
+
+
+
+
+            outputData(elements);
+           
+           
+           
+             Console.ReadLine();
+        }
+
+        /*
+         * This method takes a list and simply outputs them
+         */
+        private static void outputData(List<string> elements)
+        {
+            foreach (var item in elements)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+       
+       
+
+    }
+}
